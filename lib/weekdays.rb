e39497c6e560373ac020d25b8e06bb29bb85b2c5
Jameslittle1 <jameslittle314159@gmail.com>
2019-04-15 11:31:17 +0100

    Add Tueaday, Wednesday, Thursday + Refactor logic

--- lib/weekdays.rb
+++ lib/weekdays.rb
@@ -4,19 +4,34 @@ Dir["./errors/*.rb"].each { |file| require file }
 module Weekdays
   class Converter
     class << self
+      DAYS = { monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5 }
       def monday(date)
-        date = string_to_date(date) if date.class == String
-        raise WeekendError if weekend?(date)
-        date_to_string(date + (1 - date.wday))
+        logic(date, DAYS[:monday])
+      end
+
+      def tuesday(date)
+        logic(date, DAYS[:tuesday])
+      end
+
+      def wednesday(date)
+        logic(date, DAYS[:wednesday])
+      end
+
+      def thursday(date)
+        logic(date, DAYS[:thursday])
       end
 
       def friday(date)
-        date = string_to_date(date) if date.class == String
-        raise WeekendError if weekend?(date)
-        date_to_string(date + (5 - date.wday))
+        logic(date, DAYS[:friday])
       end
 
       private
+
+      def logic(date, num)
+        date = string_to_date(date) if date.class == String
+        raise WeekendError if weekend?(date)
+        date_to_string(date + (num - date.wday))
+      end
 
       def string_to_date(string)
         Date.parse(string)
--- spec/weekdays_spec.rb
+++ spec/weekdays_spec.rb
@@ -43,6 +43,132 @@ RSpec.describe Weekdays::Converter do
       end
     end
   end
+  describe '.tuesday' do
+    context '(\'2019-04-08\')' do
+      it 'returns 2019-04-09' do
+        expect(described_class.tuesday('2019-04-08')).to eq('2019-04-09')
+      end
+    end
+    context '(\'2019-04-09\')' do
+      it 'returns 2019-04-09' do
+        expect(described_class.tuesday('2019-04-09')).to eq('2019-04-09')
+      end
+    end
+    context '(\'2019-04-10\')' do
+      it 'returns 2019-04-09' do
+        expect(described_class.tuesday('2019-04-10')).to eq('2019-04-09')
+      end
+    end
+    context '(\'2019-04-11\')' do
+      it 'returns 2019-04-09' do
+        expect(described_class.tuesday('2019-04-11')).to eq('2019-04-09')
+      end
+    end
+    context '(\'2019-04-12\')' do
+      it 'returns 2019-04-09' do
+        expect(described_class.tuesday('2019-04-12')).to eq('2019-04-09')
+      end
+    end
+    context '(\'2019-04-13\')' do
+      it 'raises WeekendError' do
+        expect { described_class.tuesday('2019-04-13') }.to raise_error(WeekendError)
+      end
+    end
+    context '(\'2019-04-14\')' do
+      it 'raises WeekendError' do
+        expect { described_class.tuesday('2019-04-14') }.to raise_error(WeekendError)
+      end
+    end
+    context 'with invalid date' do
+      it 'raises InvalidDateError' do
+        expect { described_class.tuesday('not a date') }.to raise_error(InvalidDateError)
+      end
+    end
+  end
+  describe '.wednesday' do
+    context '(\'2019-04-08\')' do
+      it 'returns 2019-04-10' do
+        expect(described_class.wednesday('2019-04-08')).to eq('2019-04-10')
+      end
+    end
+    context '(\'2019-04-09\')' do
+      it 'returns 2019-04-10' do
+        expect(described_class.wednesday('2019-04-09')).to eq('2019-04-10')
+      end
+    end
+    context '(\'2019-04-10\')' do
+      it 'returns 2019-04-10' do
+        expect(described_class.wednesday('2019-04-10')).to eq('2019-04-10')
+      end
+    end
+    context '(\'2019-04-11\')' do
+      it 'returns 2019-04-10' do
+        expect(described_class.wednesday('2019-04-11')).to eq('2019-04-10')
+      end
+    end
+    context '(\'2019-04-12\')' do
+      it 'returns 2019-04-10' do
+        expect(described_class.wednesday('2019-04-12')).to eq('2019-04-10')
+      end
+    end
+    context '(\'2019-04-13\')' do
+      it 'raises WeekendError' do
+        expect { described_class.wednesday('2019-04-13') }.to raise_error(WeekendError)
+      end
+    end
+    context '(\'2019-04-14\')' do
+      it 'raises WeekendError' do
+        expect { described_class.wednesday('2019-04-14') }.to raise_error(WeekendError)
+      end
+    end
+    context 'with invalid date' do
+      it 'raises InvalidDateError' do
+        expect { described_class.wednesday('not a date') }.to raise_error(InvalidDateError)
+      end
+    end
+  end
+  describe '.thursday' do
+    context '(\'2019-04-08\')' do
+      it 'returns 2019-04-11' do
+        expect(described_class.thursday('2019-04-08')).to eq('2019-04-11')
+      end
+    end
+    context '(\'2019-04-09\')' do
+      it 'returns 2019-04-11' do
+        expect(described_class.thursday('2019-04-09')).to eq('2019-04-11')
+      end
+    end
+    context '(\'2019-04-10\')' do
+      it 'returns 2019-04-11' do
+        expect(described_class.thursday('2019-04-10')).to eq('2019-04-11')
+      end
+    end
+    context '(\'2019-04-11\')' do
+      it 'returns 2019-04-11' do
+        expect(described_class.thursday('2019-04-11')).to eq('2019-04-11')
+      end
+    end
+    context '(\'2019-04-12\')' do
+      it 'returns 2019-04-11' do
+        expect(described_class.thursday('2019-04-12')).to eq('2019-04-11')
+      end
+    end
+    context '(\'2019-04-13\')' do
+      it 'raises WeekendError' do
+        expect { described_class.thursday('2019-04-13') }.to raise_error(WeekendError)
+      end
+    end
+    context '(\'2019-04-14\')' do
+      it 'raises WeekendError' do
+        expect { described_class.thursday('2019-04-14') }.to raise_error(WeekendError)
+      end
+    end
+    context 'with invalid date' do
+      it 'raises InvalidDateError' do
+        expect { described_class.thursday('not a date') }.to raise_error(InvalidDateError)
+      end
+    end
+  end
   describe '.friday' do
     context '(\'2019-04-08\')' do
       it 'returns 2019-04-12' do
